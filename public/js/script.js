ecbf30e9cca38f3c29f3b94b96f86d360c4938d0
maliefrr <aliefm144@gmail.com>
2022-05-15 12:32:18 +0700

    update to porto v.2.0

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+// hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+}
+);
+
+// Navbar Fixed
+
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#c026d3",
+        dark: "#475569",
+        secondary: "#64748b",
+      },
+      screen: {
+        "2xl": "1320px",
+      },
+      fontFamily: {
+        poppins : ["Poppins", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
