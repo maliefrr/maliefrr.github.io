23a4204499f8de59f7a6dad16820d152217f1872
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-04-27 08:35:01 +0700

    feat: change portofolio to react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Welcome to My Portfolio Website</h1>
+    </div>
+  )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,37 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
   theme: {
     container: {
       center: true,
-      padding: "16px",
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
-      colors: {
-        primary: "#c026d3",
-        dark: "#475569",
-        secondary: "#64748b",
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
-      screen: {
-        "2xl": "1320px",
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
-      fontFamily: {
-        poppins : ["Poppins", "sans-serif"],
-      }
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
