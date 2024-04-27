f1a5832c946b75ba86ba34b771720b5eabb2404a
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-04-27 10:23:45 +0700

    feat: add button and separator component, add hero section and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import {useRef} from 'react';
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
 
 
 function App() {
-
+  const navbarRef = useRef<null | HTMLDivElement>(null);
+  const handleClick = () => {
+      navbarRef.current!.scrollIntoView({ behavior: "smooth" });
+  }
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Welcome to My Portfolio Website</h1>
+    <div className="overflow-x-hidden">
+      <div className="w-screen h-screen flex flex-col items-center justify-center bg-[rgba(0,0,0)]">
+        <h4 className="text-3xl">🖐</h4>
+        <h3 className="text-[#c7d5e0] text-2xl">I am Mohamad Alief Rizky Ramadhan</h3>
+        <h4 className="text-[#c7d5e0] text-3xl">A Frontend Engineer</h4>
+        <Separator className="max-w-[20%] mt-3"/>
+        <Button className="mt-3 bg-[#2a475e] hover:bg-[#1b2838] duration-300" onClick={handleClick}>Get To Know Me 👇</Button>
+    </div>
+    <div className="sticky top-0 z-50 w-full bg-[#171a21] backdrop-blur supports-[backdrop-filter]:bg-[#171a21] px-5" ref={navbarRef}>
+      <nav className="flex justify-between items-center">
+        <h1 className="text-xl text-white">Mohamad Alief Rizky R</h1>
+        <ul className="flex justify-around w-[60%] p-4 text-white">
+          <li className="hover:text-[#c7d5e0] duration-300 cursor-pointer">Home</li>
+          <li className="hover:text-[#c7d5e0] duration-300 cursor-pointer">Experience</li>
+          <li className="hover:text-[#c7d5e0] duration-300 cursor-pointer">Project</li>
+          <li className="hover:text-[#c7d5e0] duration-300 cursor-pointer">Skills</li>
+          <li className="hover:text-[#c7d5e0] duration-300 cursor-pointer">Contact</li>
+        </ul>
+      </nav>
+    </div>
+      <div className="h-[1000px]"></div>
     </div>
   )
 }
