15cb4cc12637199a156e1fca2bb9e7463bcb8e33
Moh Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2025-12-14 14:53:24 +0700

    fix: fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maliefrr.github.io',
   integrations: [react(), tailwind()]
 });
